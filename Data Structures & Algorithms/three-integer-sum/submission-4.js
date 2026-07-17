6902cfb0b1a0467504d82123edaf74d8b89227f3
IRADUKUNDA Toussaint <iradukundatoussaint01@gmail.com>
2026-07-18 00:50:37 +0200

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.js
@@ -0,0 +1,57 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  const result = [];
+
+  nums.sort((a, b) => a - b);
+
+  while (left < right) {
+    let res1 = nums[left] + nums[right];
+
+
+    if (res1 >= 0) {
+      let middle = left + 1;
+      let res2 = res1 + nums[middle];
+
+
+      while (res2 <= 0) {
+        if (res2 === 0) {
+          result.push([nums[left], nums[middle], nums[right]]);
+          break;
+        }
+        res2 -= nums[middle];
+        middle++;
+        res2 += nums[middle];
+      }
+
+      right--;
+    } else {
+      let middle = right - 1;
+
+      let res2 = res1 + nums[middle];
+
+
+
+      while (res2 >= 0) {
+
+        if (res2 === 0) {
+          result.push([nums[left], nums[middle], nums[right]]);
+          break;
+        }
+        res2 -= nums[middle];
+        middle--;
+        res2 += nums[middle];
+      }
+
+      left++;
+    }
+  }
+
+  return result;
+}}
